c9e138c46b2a8bffc5af7a1e38733f93790d964a
Mamedov Zaur <zm0963@gmail.com>
2020-11-22 21:58:37 +0300

    Thread in my function

--- programm.cpp
+++ programm.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
-#define Foo(x,y)((x)+(y))
+#include <thread>
 
-int main(){
+using namespace std;
 
-  std::cout<<Foo(3,2);
+void Sum(int &a)
+{
+  this_thread::sleep_for(chrono::milliseconds(3000));
 
-  return 0;
+  cout << "===============| Sum started |===============" << endl;
+
+  this_thread::sleep_for(chrono::milliseconds(4000));
+
+  a *= 2;
+
+  this_thread::sleep_for(chrono::milliseconds(4000));
+
+  cout << "===============| Sum stopped |===============" << endl;
+}
+
+
+
+int main()
+{
+  setlocale(LC_ALL, "ru");
+
+  int a = 3;
+
+  thread p(Sum, std::ref(a));
+
+  for (size_t i=0; i < 10; i++)
+  {
+    cout << "ID potoka: "<< this_thread::get_id() <<  "\tmain works\t" << i << endl;
+    this_thread::sleep_for(chrono::milliseconds(2000));
+  }
+
+  cout << a << endl;
+  p.join();
+
 }
