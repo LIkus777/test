904f35cde7813f4be9ca44dce9b0169fced9b351
Mamedov Zaur <zm0963@gmail.com>
2021-02-20 16:00:39 +0300

    new files

--- newfile.cpp
+++ newfile.cpp
@@ -1,1 +1,13 @@
-lol
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+      int n;
+    string s,  A[100];
+    cin >> n;
+    getline(cin, s);
+    for (int i = 0; i < n; ++i)
+      getline(cin, A[i]);
+}
